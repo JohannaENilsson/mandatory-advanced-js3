b1b75119f7f38a1f9669752df8c24cef32ea5879
Johanna Nilsson <nijodotcom@gmail.com>
2020-02-02 09:44:43 +0100

    changed to yarn. css basic styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,20 +3,28 @@ import React from 'react';
 export default function Form(props) {
   return (
     <form onSubmit={props.handleSubmit}>
-      <input
-        type='email'
-        name='email'
-        value={props.email}
-        onChange={props.handleInput}
-      />
-      <input
-        type='password'
-        name='password'
-        min='5'
-        value={props.password}
-        onChange={props.handleInput}
-      />
-      <input type='submit' value={props.submitButtonText} />
+      <div>
+        <span>Email: </span>
+        <input
+          type='email'
+          name='email'
+          value={props.email}
+          onChange={props.handleInput}
+        />
+      </div>
+      <div>
+        <span>Password: </span>
+        <input
+          type='password'
+          name='password'
+          min='5'
+          value={props.password}
+          onChange={props.handleInput}
+        />
+      </div>
+      <div>
+        <input type='submit' value={props.submitButtonText} />
+      </div>
     </form>
   );
 }
